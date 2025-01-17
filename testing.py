0dec5e2ee65ab54ba26f9f99300703e3447bccc6
Seymour <seymour.palmer0406@gmail.com>
2025-01-18 12:51:04 +1300

    initial commit

---
+++ testing.py
@@ -0,0 +1,11 @@
+from machine import Pin
+from time import sleep
+
+
+led = Pin(15, Pin.OUT)
+button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+
+while True:
+    if button.value():
+        led.toggle()
+        sleep(0.5)
